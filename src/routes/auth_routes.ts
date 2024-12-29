78c78a751127a9a3ca4936a8abf68d8935e49dd3
marlenbd <marlen@htbioimaging.com>
2024-12-29 16:30:02 +0200

    add google sign in

--- src/routes/auth_routes.ts
+++ src/routes/auth_routes.ts
@@ -7,6 +7,8 @@ router.post("/register", authController.register);
 
 router.post("/login", authController.login);
 
+router.post("/google/login", authController.googleSignin);
+
 router.post("/refresh", authController.refresh);
 
 router.post("/logout", authController.logout);
