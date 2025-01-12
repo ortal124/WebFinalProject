14e314f81d2b0662ac86fe3acf42f251f5e1d528
ortal124 <ortal120400@gmail.com>
2025-01-12 22:23:05 +0200

    add jest configs

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+export default {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    roots: ["<rootDir>/src"],
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov', 'html'], 
+  };
