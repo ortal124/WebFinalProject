fbc67e6e8ca86ca8d3f17c64910340e988383bdf
ortal124 <ortal120400@gmail.com>
2025-01-21 11:54:13 +0200

    fix comments add return

--- src/controllers/comments_controller.ts
+++ src/controllers/comments_controller.ts
@@ -42,9 +42,11 @@ export const deleteComment = async (req: Request, res: Response) => {
     const comment = await commentService.getCommentById(id);
     if (!comment) {
       res.status(404).json({ error: 'Comment not found' });
+      return
     }
     else if(comment.userId != userId) {
         res.status(401).json({ error: 'User is not authorized to delete comment of another user' });
+        return
     }
     await commentService.deleteComment(id);
     res.status(200).json({ message: 'Comment deleted successfully' });
