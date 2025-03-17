416e531577d81235ff5c2b0431692c243fbfac88
ortal124 <ortal120400@gmail.com>
2025-03-18 00:58:34 +0200

    add https

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
 import initApp from "./server";
 import dotenv from "dotenv"
+import http from 'http';
+import * as https from 'https';
+import fs from 'fs';
 
 dotenv.config();
 
-const port = process.env.PORT || 3000;
-
 initApp().then((app) => {
-  app.listen(port, () => {
-    console.log(`app listening at http://localhost:${port}`);
-  });
+  if(process.env.NODE_ENV != 'production') {
+    console.log('development');
+    http.createServer(app).listen(process.env.PORT);
+    console.log(`app listening at http://localhost:${process.env.PORT}`);
+  }
+  else{
+    const options: https.ServerOptions = {
+      key: fs.readFileSync('./client-key.pem'),
+      cert: fs.readFileSync('./client-cert.pem')
+    }
+    https.createServer(options, app).listen(process.env.HTTPS_PORT);
+    console.log(`app listening at https://localhost:${process.env.HTTPS_PORT}`);
+  }
 });
